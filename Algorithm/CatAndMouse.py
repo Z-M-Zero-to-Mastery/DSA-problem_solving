c49b2172de3c142550cf71830b2332ca04854ffd
AthulKkumar <athulkkumar5@gmail.com>
2023-07-03 14:40:28 +0530

    FInding which cat catch mouse first.

---
+++ Algorithm/CatAndMouse.py
@@ -0,0 +1,42 @@
+# Two cats and a mouse are at various positions on a line. You will be given their starting positions. Your task is to determine which cat will reach the mouse first, assuming the mouse does not move and the cats travel at equal speed. If the cats arrive at the same time, the mouse will be allowed to move and it will escape while they fight.
+# You are given q queries in the form of x, y, and z representing the respective positions for cats A and B, and for mouse C. Complete the function catAndMouse to return the appropriate answer to each query, which will be printed on a new line.
+# If cat A catches the mouse first, print Cat A.
+# If cat B catches the mouse first, print Cat B.
+# If both cats reach the mouse at the same time, print Mouse C as the two cats fight and mouse escapes.
+# Example
+# x = 2
+# y = 5
+# z = 4
+# The cats are at positions 2 (Cat A) and 5 (Cat B), and the mouse is at position 4. Cat B, at position 5 will arrive first since it is only 1 unit away while the other is 2 units away. Return 'Cat B'.
+# Sample Input 0
+# 2
+# 1 2 3
+# 1 3 2
+# Sample Output 0
+# Cat B
+# Mouse C
+# Explanation 0
+# Query 0: The positions of the cats and mouse are shown below: image
+
+#          (catB)
+# 0 -> 1 -> 2 -> 3
+#    (catA)     (mouseC)
+
+# Cat B will catch the mouse first, so we print Cat B on a new line.
+
+
+# Solution: 1
+def catAndMouse(x, y, z):
+    if abs(x - z) == abs(y - z):
+        return "Mouse C"
+    elif abs(x - z) > abs(y - z):
+        return "Cat B"
+    else:
+        return "Cat A"
+
+
+# Input
+x = 1
+y = 2
+z = 3
+print(catAndMouse(x, y, z))
